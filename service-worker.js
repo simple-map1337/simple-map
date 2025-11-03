e4107447724186f9765ce537b5e63db412e50aac
simple-map1337 <soft2281337666@gmail.com>
2025-11-03 15:33:22 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+// service-worker.js
+const CACHE_NAME = 'targets-map-v1.1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/111.html',
+  '/manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Возвращаем кэшированную версию или загружаем новую
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
